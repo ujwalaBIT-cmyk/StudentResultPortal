74d72403668f89f5c3e51cc69f6a3ccddcc1cf1d
Bit21 <ujwalamohane@bitbaroda.com>
2026-09-21 18:29:19 +0530

    Add class model and database migration

--- studentresult/subjects/apps.py
+++ studentresult/subjects/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class SubjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'subjects'
+
+
+    
--- studentresult/subjects/models.py
+++ studentresult/subjects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+    subject_code = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.subject_code} - {self.subject_name}"
