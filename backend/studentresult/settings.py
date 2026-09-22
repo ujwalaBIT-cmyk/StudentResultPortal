d9dfa07c54db249c39b2a5b0a3b9d371cb0fd02d
Bit21 <ujwalamohane@bitbaroda.com>
2026-09-22 15:50:11 +0530

    Separate Django backend and frontend structure

--- backend/studentresult/settings.py
+++ backend/studentresult/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'studentresult.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / 'frontend' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR.parent / 'frontend' / 'static',
+]
